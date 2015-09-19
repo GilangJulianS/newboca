6a32099f3c633bfbba0004208de10d5405715f64
Gilang Julian <gilang.9h@gmail.com>
2015-09-19 15:19:35 +0700

    finished mockup for book list

--- app/src/main/java/com/boca/boca/BookingListFragment.java
+++ app/src/main/java/com/boca/boca/BookingListFragment.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by gilang on 17/09/2015.
@@ -15,6 +19,8 @@ import android.view.ViewGroup;
 public class BookingListFragment extends Fragment {
 
 	private Toolbar toolbar;
+	private TextView btnBook;
+	private TextView btnTopup;
 
 	public BookingListFragment(){}
 
@@ -29,11 +35,31 @@ public class BookingListFragment extends Fragment {
 		toolbar = (Toolbar) v.findViewById(R.id.toolbar);
 		ViewGroup container = (ViewGroup) v.findViewById(R.id.list_container);
 		((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+		btnBook = (TextView) v.findViewById(R.id.btn_book);
+		btnBook.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-		for(int i=0; i<10; i++){
+			}
+		});
+		btnTopup = (TextView) v.findViewById(R.id.btn_topup);
+		btnTopup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		final int size = 10;
+		final List<ViewGroup> hiddenLayouts = new ArrayList<>();
+		for(int i=0; i<size; i++){
 			View item = inflater.inflate(R.layout.item_book_list, container, false);
 			CardView card = (CardView) item.findViewById(R.id.card);
+			ViewGroup g = (ViewGroup) card.findViewById(R.id.detail_view);
+			hiddenLayouts.add(g);
+			final int id = i;
 			card.setOnClickListener(new View.OnClickListener() {
+				int listenerid = id;
 				@Override
 				public void onClick(View v) {
 					ViewGroup view = (ViewGroup) v.findViewById(R.id.detail_view);
@@ -41,6 +67,11 @@ public class BookingListFragment extends Fragment {
 						view.setVisibility(View.GONE);
 					}else{
 						view.setVisibility(View.VISIBLE);
+						for(int j=0; j<size; j++){
+							if(j != listenerid){
+								hiddenLayouts.get(j).setVisibility(View.GONE);
+							}
+						}
 					}
 				}
 			});
